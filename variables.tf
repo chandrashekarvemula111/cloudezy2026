3d4d879a451a192564e4f2f95331c2a4c138eeec
Vemula Shekhar <vemulashekhar@ad.corp.google.com>
2026-03-12 05:10:39 +0530

    updated varialble configuration in root module configuration file

--- variables.tf
+++ variables.tf
@@ -16,3 +16,29 @@ variable "bucket_name" {
 }
 */
 
+variable "gce_name" {
+    type = string
+    description = "name of the gce instance"
+}
+variable "instance-type" {
+    type = string
+    default = "e2-micro"
+    description = "to specify instance type"
+}
+variable "lables" {
+    default {
+        terraform = true
+        project = cloudezy
+        environment = cloudezy
+        
+    }
+}
+variable "backend-bucket" {
+    type = string
+    description = "GCS bucket for terraform state"
+    
+}
+variable "private-ip" {
+    type = string
+    description = "to Print private ip of instanace"
+}
