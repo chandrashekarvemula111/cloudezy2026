d9444304b17b4d17796e61d4489f1d6002a75f83
Vemula Shekhar <vemulashekhar@ad.corp.google.com>
2026-04-21 18:25:28 +0530

    new test

---
+++ test/main.tf
@@ -0,0 +1,38 @@
+resource "google_service_account" "default" {
+  account_id   = data.terraform_sa
+  display_name = "Custom SA for VM Instance"
+}
+
+resource "google_compute_instance" "instance" {
+  name             = var.gce_name
+  zone             = "us-central1-a"
+  machine_type     = var.instance-type
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-os-cloud/ubuntu-2204-lts"
+      labels = {
+        var.labels
+      }
+    }
+  }
+
+  // Local SSD disk
+  scratch_disk {
+    interface = "NVME"
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral public IP
+    }
+  }
+
+  service_account {
+    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
+    email  = google_service_account.default.email
+    scopes = ["cloud-platform"]
+  }
+}
